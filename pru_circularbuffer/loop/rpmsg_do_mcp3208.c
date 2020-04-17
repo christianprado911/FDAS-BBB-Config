1b045051b88dd0292420336a5c95b57957be90ef
christianprado911 <52748683+christianprado911@users.noreply.github.com>
2020-04-17 16:37:46 -0300

    Update rpmsg_do_mcp3208.c

--- pru_circularbuffer/loop/rpmsg_do_mcp3208.c
+++ pru_circularbuffer/loop/rpmsg_do_mcp3208.c
@@ -38,21 +38,19 @@
 #include <string.h>
 #include <sys/poll.h>
 
-/*  CONSTANTS OF CIRCULAR BUFFER */
+#define		BUFFER_PRU		100
+#define		MAX_BUFFER_SIZE		10000000	// 10 segundos (PRU 1MHz)
+#define		CIRCULAR_BUFFER		1000000		// 1 Segundo (PRU 1MHz)
+#define		NUM_MESSAGES		10000000 //apenas para teste
+#define		DEVICE_NAME		"/dev/rpmsg_pru123"
 
-#define		MAX_BUFFER_SIZE		10000000	// 10 segundos 
-#define		CIRCULAR_BUFFER		1000000		// 1 Segundo
-
-	int	readBuf[CIRCULAR_BUFFER];
+	int	readBuf[BUFFER_PRU];
 	int	circularBuffer[MAX_BUFFER_SIZE] = { 0 };	// Empty circular buffer
 	int	readIndex	=	0;	// Index of the read pointer
 	int	writeIndex	=	0;	// Index of the write pointer
 	int	bufferLength	=	0;	// Number of values in circular buffer
 	int 	trRead		=	0;  	// Trigger to readBuf
 	int 	trigger		=	0;  	// Trigger to measure
-
-#define NUM_MESSAGES		10000000 //apenas para teste
-#define DEVICE_NAME		"/dev/rpmsg_pru123"
 
 int main(void)
 {
@@ -100,7 +98,7 @@ int main(void)
 			//	printf("Message %d: Sent to PRU\n", i);
 
 			/* Poll until we receive a message from the PRU */
-			result = read(pollfds[0].fd, readBuf, MAX_BUFFER_SIZE);
+			result = read(pollfds[0].fd, readBuf, BUFFER_PRU);
 
 			/* Read the buffer and print it if necessary */
 			if (result > 0) {
