c94e2be65117ef618c2601e6ffcbc0440d61cec3
christianprado911 <52748683+christianprado911@users.noreply.github.com>
2020-07-02 17:12:57 -0300

    Update teste_bit.c

--- pru_circularbuffer/pru0_mcp3208ch/teste_bit.c
+++ pru_circularbuffer/pru0_mcp3208ch/teste_bit.c
@@ -5,15 +5,15 @@ int main (void){
 
 int i = 0;
 uint8_t BIT = 0b1000;
-BIT|=1;
-
+for(int i =0; i<=3; i++){
 	while (BIT) {
     	if (BIT & 1)
         	printf("1");
     		else
         	printf("0");
     		BIT >>= 1;
-		i++;
 	}
-	printf("\n %d \n", i);
+	printf("\n"); 
+	BIT|=i;
+}
 }
