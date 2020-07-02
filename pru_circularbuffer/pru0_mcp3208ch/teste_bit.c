09fa92afd1f7a9ffe176050455870a8af56f7678
christianprado911 <52748683+christianprado911@users.noreply.github.com>
2020-07-02 15:44:25 -0300

    Update teste_bit.c

--- pru_circularbuffer/pru0_mcp3208ch/teste_bit.c
+++ pru_circularbuffer/pru0_mcp3208ch/teste_bit.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 int main (void){
-
+	static unsigned char mask[] = {128, 64, 32, 16, 8, 4, 2, 1};
 uint8_t BIT = 0b1000;
 BIT|=7;
 	while (BIT) {
@@ -14,5 +14,11 @@ BIT|=7;
     		BIT >>= 1;
 	}
 	printf("\n");
+
+    if ((BIT & mask[0]) != 0) {
+	    pritf("O bit eh um");
+	    else
+		    printf("O bit eh zero);
+	}
 
 }
