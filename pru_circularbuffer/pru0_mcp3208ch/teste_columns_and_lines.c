31cda7ef1c6358d26c565db63bcdf3114eeb7e04
christianprado911 <52748683+christianprado911@users.noreply.github.com>
2020-07-05 16:08:12 -0300

    Update teste_columns_and_lines.c

--- pru_circularbuffer/pru0_mcp3208ch/teste_columns_and_lines.c
+++ pru_circularbuffer/pru0_mcp3208ch/teste_columns_and_lines.c
@@ -17,18 +17,35 @@ int * getRandom( ) {
    return r;
 }
 
+/* function to print a file with values */
+void buffer1 (int d[], int ch, int n)
+{
+	char buf[30];
+	snprintf(buf, sizeof buf,  "print-columns.txt");
+	int i = 0, k = 0;
+	FILE *fl1;j
+	fl1 = fopen(buf, "w");
+
+	for(i = 0; i <= n/ch; i++){
+    for(int j = 0; j <= ch; j++){
+		fprintf(fl1, "%d\n", d[k++]);
+	}}
+	fclose(fl1);
+}
+
 /* main function to call above defined function */
 int main () {
 
+   int n = 1000, ch = 10;
    /* a pointer to an int */
-   int *p;
-   int i;
-
-   p = getRandom();
+   int *p[];
 	
-   for ( i = 0; i < 10; i++ ) {
+   for (int i = 0; i < n; i++ ) {
+     p[i] = getRandom();
       printf( "*(p + %d) : %d\n", i, *(p + i));
    }
 
+   buffer1(p, ch, n )
+	
    return 0;
 }
