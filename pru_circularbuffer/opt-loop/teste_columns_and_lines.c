2c9a232b5969dc6613d33bf62e2ed4b7e332e2f8
christianprado911 <christian.fisica@gmail.com>
2020-07-06 02:48:57 +0000

    new file:   print-columns.txt 	new file:   teste_columns 	new file:   teste_columns_and_lines.c 	deleted:    ../pru0_mcp3208ch/print-columns.txt 	deleted:    ../pru0_mcp3208ch/teste_columns 	deleted:    ../pru0_mcp3208ch/teste_columns_and_lines.c

---
+++ pru_circularbuffer/opt-loop/teste_columns_and_lines.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* function to print a file with values */
+void buffer1 (uint8_t d[], int ch, int n)
+{
+	char buf[30];
+	snprintf(buf, sizeof buf,  "print-columns.txt");
+	int i = 0, k = 0;
+	FILE *fl1;
+	fl1 = fopen(buf, "w");
+
+	for(i = 0; i < n/ch; i++){
+    		for(int j = 0; j < ch; j++){
+			fprintf(fl1, "%3d\t", d[k++]);
+	}fprintf(fl1, "\n");}
+	fclose(fl1);
+}
+
+/* main function to call above defined function */
+int main () {
+
+   int n = 1000, ch = 10;
+   /* a pointer to an int */
+   uint8_t p[n];
+   srand( (unsigned)time( 0 ) );
+   
+	for (int i = 0; i <= n; i++ ) {
+     		p[i] = rand();
+      //printf( "*(p + %d) : %d\n", i, *(p + i));
+   }
+
+   buffer1( p, ch, n );
+
+   return 0;
+}
