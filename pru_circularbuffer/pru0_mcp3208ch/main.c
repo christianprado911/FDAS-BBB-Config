249ad5b5e4ecaacc8948e079d1b547e2d45826d7
christianprado911 <52748683+christianprado911@users.noreply.github.com>
2020-07-05 23:38:00 -0300

    Update main.c

--- pru_circularbuffer/pru0_mcp3208ch/main.c
+++ pru_circularbuffer/pru0_mcp3208ch/main.c
@@ -211,7 +211,7 @@ void main(void)
 
 	/* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
 	while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
-        while(1) {
+        //while(1) {
 		/* Check bit 31 of register R31 to see if the ARM has kicked us */
 		if (__R31 & HOST_INT) {
 			/* Clear the event status */
@@ -227,6 +227,6 @@ void main(void)
                           pru_rpmsg_send(&transport, dst, src, buffer, sizeof buffer);
                         }
 		}
-        }
+       // }
 	__halt();
 }
