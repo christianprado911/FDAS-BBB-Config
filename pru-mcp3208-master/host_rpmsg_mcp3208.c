704873943fe60ac7ec4da92d7ad793f1d1219365
christianprado911 <52748683+christianprado911@users.noreply.github.com>
2020-07-27 03:28:27 -0300

    Update host_rpmsg_mcp3208.c
    Continuar na linha tracejada a implementação do temporizador e verificar impressao dos valores em 4 colunas por linha a partir da contagem inicial em qualquer canal.

--- pru-mcp3208-master/host_rpmsg_mcp3208.c
+++ pru-mcp3208-master/host_rpmsg_mcp3208.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <time.h>
 
 uint8_t readBuf[512];
 #define DEVICE_NAME  "/dev/rpmsg_pru1"
@@ -16,15 +17,58 @@ uint8_t readBuf[512];
 #define NUM_SCANS          60
 #define DATA_BUFFER_LEN    (NUM_SCAN_ELEMENTS * NUM_SCANS)
 #define BUFFER_WORD_LEN    2 + DATA_BUFFER_LEN/2
+#define		MAX_BUFFER_SIZE		10000000000	// 10 segundos
+#define		CIRCULAR_BUFFER		1000000000		// 1 Segundo (nanosegundos)
+
+int ch[NUM_SCAN_ELEMENTS][CIRCULAR_BUFFER] = { 0 };	// Empty circular buffer
+int buffer_10s[MAX_BUFFER_SIZE] = { 0 };	// Empty 10s buffer
+int readIndex =	0;	// Index of the read pointer
+int writeIndex =	0;	// Index of the write pointer
+int bufferLength =	0;	// Number of values in circular buffer
+int count = 0;  	// Index of buffer_10s / end of loop
+int trigger = 0;  	// Trigger to change from circular buffer to 10s buffer
+char namefile[20]		;	// Record the date of the signal
 
 typedef struct {
   uint64_t timestamp_ns;
   uint16_t data[DATA_BUFFER_LEN];
 } Buffer;
 
-  uint16_t ch[NUM_SCAN_ELEMENTS][NUM_SCANS];
+//Função que grava os arquivos do buffer circular
+void buffer1 (int d[][], int n, char namefile[])
+{
+	char buf[30];
+	snprintf(buf, sizeof buf,  "%s-1.txt", namefile);
+	int i = 0;
+	FILE *fl1;
+	fl1 = fopen(buf, "w");
+
+	for(i = 0; i <= ; i++){
+		fprintf(fl1, "%d\n", d[i]);
+	}
+	fclose(fl1);
+}
+/* Fim da impressão do Buffer circular */
+
+//Função que grava os arquivos do buffer_10s
+void buffer10 (int d[], int n, char namefile[])
+{
+	char buf[30];
+	snprintf(buf, sizeof buf,  "%s-2.txt", namefile);
+	int i = 0;
+	FILE *fl2;
+	fl2 = fopen(buf, "w");
+
+	for(i = 0; i <= n; i++){
+		fprintf(fl2, "%d\n", d[i]);
+	}
+	fclose(fl2);
+}
 
 int main(void) {
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+
   /* Open the rpmsg_pru character device file */
   int fd = open(DEVICE_NAME, O_RDWR);
   if (fd < 0) {
@@ -44,16 +88,38 @@ int main(void) {
     result = read(fd, readBuf, sizeof readBuf);
     if (result == sizeof(Buffer)) {
       Buffer *b = (Buffer *) readBuf;
-      for (int i=0; i<DATA_BUFFER_LEN; i++){
-      ch[i % NUM_SCAN_ELEMENTS][i % NUM_SCANS] = b->data[i];
-        printf("ch%d=%4" PRIu16 ", ", i % NUM_SCAN_ELEMENTS , ch[i % NUM_SCAN_ELEMENTS][i % NUM_SCANS]);
+      for (int i=0; i<DATA_BUFFER_LEN; i++){ 
+      /* printf("ch%d=%4" PRIu16 ", ", i % NUM_SCAN_ELEMENTS , ch[i % NUM_SCAN_ELEMENTS][i % NUM_SCANS]);
       if(i % NUM_SCAN_ELEMENTS == NUM_SCAN_ELEMENTS - 1){
       printf("ts=%" PRIu64 ",\t", b->timestamp_ns);
       printf("delta=%" PRIu64, b->timestamp_ns - last_ts);
-      printf("\n");}
-        
-      }
-      last_ts = b->timestamp_ns;
+      printf("\n");} //apenas teste */ 
+       if( trigger == 0 && b->data[i] > 1500 ) {
+         trigger = 1;
+         snprintf(namefile, sizeof namefile, "%02d-%02d-%d_%02dh%02dm%02d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+         printf("Pulso Capturado, trigger %d\n\n", trigger);
+         last_ts = b->timestamp_ns;}
+        /* Inicio da coleta dos 10 segundos real-time */
+        if(trigger == 0){
+        ch[i % NUM_SCAN_ELEMENTS][writeIndex % CIRCULAR_BUFFER] = b->data[i];
+        writeIndex++;
+        if(b->timestamp_ns - last_ts < CIRCULAR_BUFFER){ //-----------------------------------------------------------
+          bufferLength++;}
+        if (writeIndex == bufferLength) {
+          writeIndex = 0;}
+        }
+        /* Inicio da coleta dos 10 segundos real-time */
+        if(trigger == 1){
+          buffer_10s[count] = b->data[i];
+          count++;}/* Fim Coleta */
+     } //fim da captura de um buffer da pru0
+      //last_ts = b->timestamp_ns;//apenas para teste
+      if(count == MAX_BUFFER_SIZE){
+        printf("Impressao dos valores de buffer de 1s\n");
+        buffer1(ch, bufferLength, namefile); 
+        printf("Fim da impressao buffer 1 s \n Impressao dos valores de buffer 10s\n");
+        buffer10(buffer_10s, count, namefile);
+        printf("Fim da impressao");}
     } else if (result < 0) {
       perror("Error reading from device");
       return -1;
